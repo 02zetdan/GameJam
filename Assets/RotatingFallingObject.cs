9ad358db8b4a257a28ca2d4f969f964cb17d379e
Zeth Danielsson <zeda21@student.bth.se>
2024-11-16 17:30:19 +0100

    Falling Object

---
+++ Assets/RotatingFallingObject.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingFallingObject : MonoBehaviour
+{
+    public float rotationSpeed = 100f; // Speed of rotation in degrees per second
+    public float fallSpeed = 2f;       // Speed of falling
+    private bool hasLanded = false;   // Flag to check if the square has landed
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

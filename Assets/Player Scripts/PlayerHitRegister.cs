925f48a0b5e5a9c8f3fab2ec6c1d5fa6da608540
Simon Johansson <sijh21@student.bth.se>
2024-11-17 15:27:31 +0100

    Hit Effects For Frying Pan and Rolling Pin

--- Assets/Player Scripts/PlayerHitRegister.cs
+++ Assets/Player Scripts/PlayerHitRegister.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerHitRegister : MonoBehaviour
 {
     PlayerMainScript pmScript;
     public GameObject AudioManager;
+
+    float stunTimer = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +19,11 @@ public class PlayerHitRegister : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (stunTimer <  0)
+        {
+            pmScript.isStunned = false;
+        }
+        stunTimer -= Time.deltaTime;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -25,8 +33,21 @@ public class PlayerHitRegister : MonoBehaviour
             return;
         }
 
+        RotatingFallingObject rfo = collision.gameObject.GetComponent<RotatingFallingObject>();
 
-        FindObjectOfType<AudioManager>().Play("");
+        if (rfo.pickup == Pickup.FryingPan)
+        {
+            FindObjectOfType<AudioManager>().Play("FryingPanHit");
+            stunTimer = 1.5f;
+            pmScript.isStunned = true;
+        }
+        else if (rfo.pickup == Pickup.RollingPin)
+        {
+            FindObjectOfType<AudioManager>().Play("RollingPinHit");
+            stunTimer = 1f;
+            pmScript.isStunned = true;
+        }
+
 
 
     }
